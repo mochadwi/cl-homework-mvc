4bcf5e666d6188fabad4446338be1f11f637f51c
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2016-11-25 18:55:38 +0700

    [HOMEWORK] Initial Commit - RecyclerView and MVC

---
+++ app/src/main/java/io/github/mochadwi/practicerecyclerview/utility/UrlTag.java
@@ -0,0 +1,12 @@
+package io.github.mochadwi.practicerecyclerview.utility;
+
+/**
+ * Created by mochadwi on 11/18/16.
+ */
+
+public class UrlTag {
+    videoUrl = "https://www.youtube.com/watch?v=ePpPVE-GGJw&";
+//    public static String baseUrl = "http://bukuapi.azurewebsites.net/api";
+    public static String baseUrl = "http://ibacor.com/api/youtube-converter?u=" + videoUrl + "&k=f0b4e6a00b7bf989baed3e2e76a0ba7f";
+    public static String buku = baseUrl + "/buku";
+}
